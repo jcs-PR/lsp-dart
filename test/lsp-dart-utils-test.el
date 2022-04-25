8f49fb67b57844ec2e66d687f4e6d8218ce0df7f
Jen-Chieh Shen <jcs090218@gmail.com>
2022-04-25 19:34:38 +0800

    Use join instead

--- test/lsp-dart-utils-test.el
+++ test/lsp-dart-utils-test.el
@@ -76,7 +76,7 @@
    (mock (lsp-dart-flutter-project-p) => t)
    (mock (lsp-dart-get-flutter-sdk-dir) => "/flutter-sdk")
    (mock (file-exists-p "/flutter-sdk/bin/cache/dart-sdk/") => t)
-   (should (equal (lsp-dart-get-sdk-dir) (f-expand "/flutter-sdk/bin/cache/dart-sdk/" (f-root))))))
+   (should (equal (lsp-dart-get-sdk-dir) (f-join (f-root) "/flutter-sdk/bin/cache/dart-sdk/")))))
 
 (ert-deftest lsp-dart-get-sdk-dir--project-without-dart-on-path-test ()
   (lsp-dart-test-from-dart-project
